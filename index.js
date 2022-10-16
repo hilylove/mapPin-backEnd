fd5093748af929594fd0af185a2cf2ac5b5b32ab
Ashley <ashleyshi006@gmail.com>
2022-10-16 11:07:56 -0400

    fix cors problem

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const dotenv = require("dotenv");
 const app = express();
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
+const cors = require("cors");
 
 dotenv.config();
 
 app.use(express.json());
+
+const whitelist = [
+  "http://localhost:3000",
+  "https://comforting-griffin-fbd4f0.netlify.app/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  Credential: true,
+};
+
+app.use(cors(corsOptions));
 
 mongoose
   .connect(process.env.MONGO_URL, {
